03fadece1f60db42836ec3fdb9a99efb4bda1e19
Dovile Sukiene <do.leskauskaite@gmail.com>
2022-08-04 12:37:06 +0300

    Update

---
+++ TASK4_07.26/scripts/script.js
@@ -0,0 +1,24 @@
+let N1 = prompt("Please enter number 1");
+let N2 = prompt("Please enter number 2");
+
+let A1 = (Number(N1) + Number(N2));
+let A2 = (Number(N1) - Number(N2));
+let A3 = (Number(N1) * Number(N2));
+let A4 = (Number(N1) / Number(N2));
+
+alert("Results are: \nAddition: " + A1 + "\nSubtraction: " + A2 + "\nMultiplication: " + A3 + "\nDivision: " + A4 + "");
+
+//if (Number(N1) != NaN & Number(N2) != NaN) {
+//  alert("Results are: \nAddition: " + A1 + "\nSubtraction: " + A2 + "\nMultiplication: " + A3 + "\nDivision: " + A4 + "");
+//} else {
+//  alert("Numbers are not entered!");
+//}
+
+let p1 = prompt("What is your name?");
+alert("Hello " + p1 + "! \nHow are you today?");
+
+//if (p1 != null) {
+//  alert("Hello " + p1 + "! \nHow are you today?");
+//} else {
+//  alert("Name is not entered");
+//}
