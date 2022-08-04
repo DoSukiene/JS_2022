ea3b79c6ab8cf4f7570ede157adfdea4479aeb63
Dovile Sukiene <do.leskauskaite@gmail.com>
2022-08-04 13:14:04 +0300

    Update

--- TASK5_07.28/scripts/script.js
+++ TASK5_07.28/scripts/script.js
@@ -1,26 +1,43 @@
 let nu1 = prompt("Please enter first number");
-let nu2 = promt("Please enter second number");
-let action = promt("Please enter action: +, -, *, /");
-const notNumbers = isNan(nu1) || isNana(nu2);
+let nu2 = prompt("Please enter second number");
+let action = prompt("Please enter action: +, -, *, /");
+const notNumbers = isNaN(nu1) || isNaN(nu2);
 
 
 if (action == "+") {
-    if (notNumbers){
-    result = nu1 + nu2;
+    if (notNumbers) {
+        result = nu1 + nu2;
+        alert("Result is " + result);
     } else {
-    result = Number(nu1) + Number(nu2);
+        result = Number(nu1) + Number(nu2);
+        alert("Result is " + result);
     }
 
 }
 else if (action == "-") {
-    alert(Subtraction is possible only for numbers);
+    if (notNumbers) {
+        alert("Subtraction is possible only for numbers");
+    } else {
+        result = Number(nu1) - Number(nu2);
+        alert("Result is " + result);
+    }
 }
 else if (action == "*") {
-
+    if (notNumbers) {
+        alert("Multiplication is possible only for numbers");
+    } else {
+        result = Number(nu1) * Number(nu2);
+        alert("Result is " + result);
+    }
 }
 else if (action == "/") {
-
+    if (notNumbers) {
+        alert("Divission is possible only for numbers");
+    } else {
+        result = Number(nu1) / Number(nu2);
+        alert("Result is " + result);
+    }
 }
 else {
-    alert(Inserted action is not correct")
+    alert("Inserted action is not correct");
 }
